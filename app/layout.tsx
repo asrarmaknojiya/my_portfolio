e181ce9ed401d5d49bc09dad5bf928cf001f2a4a
asrarmaknojiya <asrarjabir786@gmail.com>
2025-11-29 15:02:20 +0530

    asrar

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,22 @@ html {
   --font-mono: ${GeistMono.variable};
 }
         `}</style>
+        <meta name="google-site-verification" content="t1KTM_KgajzM11fgv4NGU_MS-t1Ho7OwnICDWzFcqmQ" />
+        <title>Asrar Maknojiya — Full Stack Developer</title>
+        <meta name="title" content="Asrar Maknojiya — Full Stack Developer" />
+        <meta name="description" content="I craft modern web apps with React, Node.js, Express, MySQL, and more. Full-stack developer building high-performance digital experiences." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://asrarmaknojiya.vercel.app/" />
+        <meta property="og:title" content="Asrar Maknojiya — Full Stack Developer" />
+        <meta property="og:description" content="Full-stack developer building smooth, scalable, and secure web applications." />
+        <meta
+          name="keywords"
+          content="Asrar Maknojiya, Full Stack Developer, Web Developer, React Developer, Node.js Developer, Express.js, MySQL, JavaScript Developer, MERN Developer, API Developer, Frontend Developer, Backend Developer"
+        />
+
+
+        <link rel="canonical" href="https://asrarmaknojiya.vercel.app/" />
       </head>
       <body>{children}</body>
     </html>
